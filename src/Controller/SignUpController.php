3c189f891a2491948ee8f6033951c942b18b57bc
Jeffeos <geoffrey.delenne@gmail.com>
2020-04-17 09:51:55 +0200

    added show function

--- src/Controller/SignUpController.php
+++ src/Controller/SignUpController.php
@@ -3,6 +3,8 @@
 
 namespace App\Controller;
 
+use App\Model\SignUpManager;
+
 class SignUpController extends AbstractController
 {
 
@@ -18,4 +20,22 @@ class SignUpController extends AbstractController
     {
         return $this->twig->render('SignUp/signup.html.twig');
     }
+
+    /**
+     * Display item informations specified by $id
+     *
+     * @param int $id
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function show(int $id)
+    {
+        $signUp = new SignUpManager();
+        $signUp = $signUp->selectOneById($id);
+
+        return $this->twig->render('SignUp/signup.html.twig', ['signUp' => $signUp]);
+    }
+
 }
---
+++ src/Model/SignUpManager.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Model;
+
+
+class SignUpManager extends AbstractManager
+{
+    /**
+     *
+     */
+    const TABLE = 'USER';
+
+    /**
+     *  Initializes this class.
+     */
+    public function __construct()
+    {
+        parent::__construct(self::TABLE);
+    }
+
+}
