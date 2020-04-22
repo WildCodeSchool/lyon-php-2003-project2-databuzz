17606a5d7223baaa6eca9cf4c3091ec5ab4acb4e
Lucas Marguiron <lucas.marguiron@gmail.com>
2020-04-22 11:29:16 +0200

    Buzz and unbuzz TVshows

--- src/Model/TvshowManager.php
+++ src/Model/TvshowManager.php
@@ -27,7 +27,7 @@ class TvshowManager extends AbstractManager
         parent::__construct(self::TABLE);
     }
 
-    public function getGenre(int $id)
+    public function getGenres(int $id)
     {
         // prepared request
         $statement = $this->pdo->prepare("
@@ -40,4 +40,34 @@ class TvshowManager extends AbstractManager
 
         return $statement->fetchAll();
     }
+
+    public function buzzTvShow (int $showId, int $userId){
+        $statement = $this->pdo->prepare("INSERT INTO buzz VALUES (:userid,:showid);");
+        $statement->bindValue('userid', $userId,\PDO::PARAM_INT);
+        $statement->bindValue('showid', $showId,\PDO::PARAM_INT);
+        $statement->execute();
+    }
+
+    public function unbuzzTvShow (int $showId, int $userId){
+        $statement = $this->pdo->prepare("DELETE FROM buzz WHERE user_id=:userid AND tvshow_id=:showid;");
+        $statement->bindValue('userid', $userId,\PDO::PARAM_INT);
+        $statement->bindValue('showid', $showId,\PDO::PARAM_INT);
+        $statement->execute();
+    }
+
+    public function isBuzzed(int $showId, int $userId){
+        $statement = $this->pdo->prepare("SELECT * FROM buzz WHERE user_id=:userId AND tvshow_id=:showId;");
+        $statement->bindValue('userId', $userId,\PDO::PARAM_INT);
+        $statement->bindValue('showId', $showId,\PDO::PARAM_INT);
+        $statement->execute();
+        $result=$statement->fetch();
+
+        if($result){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+
+
 }
